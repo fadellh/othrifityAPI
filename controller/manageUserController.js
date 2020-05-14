3b84100ce10486585b56e7148c8811d6066d4248
fadellh <fadellhcoba2@gmail.com>
2020-05-14 15:41:00 +0700

    filtering api

--- controller/manageUserController.js
+++ controller/manageUserController.js
@@ -2,14 +2,35 @@ const {db,dba} = require('../database');
 
 module.exports = {
     getAllUser : async (req,res) => {
+        let {sort, filterBy1,filterBy2, filterParam2, orderBy,filterParam1,search} = req.query
+
         let sql = `select us.status_name as status, u.id, u.username, u.email , u.address, sum(t.total_price) as revenue_toko, u.join_date, count(t.userId) as total_sale 
         from users u 
         Join transaction t
         on u.id=t.userId
         join userstatus us
-        on u.statusId=us.id_status
-        group by t.userId
-        limit 10;`
+        on u.statusId=us.id_status`
+        
+        if(!orderBy&!filterBy1&!search&!filterBy2){
+            sql += ` group by t.userId limit 10`
+        }
+        if(filterBy1){
+            sql += ` where ${filterBy1} = ${filterParam1}`
+        }
+        // if(filterBy2){
+        //     sql += ` AND ${filterBy2} = ${filterParam2}`}
+        if(search){
+            {filterBy1?sql+=` AND u.username LIKE '${search}%'`:sql += ` where u.username LIKE '${search}%'`}
+        }
+        
+        if(filterBy1||filterBy2||orderBy||search){
+            sql += ` group by t.userId`
+        }
+        
+        if(orderBy){
+            sql += ` order by ${orderBy} ${sort} limit 10` 
+        }
+
         try{ 
             let result = await dba(sql)
             // console.log(result)
